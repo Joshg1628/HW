5b11ca117fec8f648830f2aecf8f57a76fa4388a
Shia Grosinger <Joshgrosinger@gmail.com>
2026-06-16 22:36:01 +0300

    Add Csharp_Quiz and Shirt class

---
+++ html/48/Csharp_Quiz.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace Class48_
+{
+    internal class Csharp_Quiz
+    {
+        static void Main(string[] args)
+        {
+            int index = 1;
+
+            foreach (Colors col in Enum.GetValues(typeof(Colors))) // loop through color enums
+            {
+                foreach (Patterns patt in Enum.GetValues(typeof(Patterns)))
+                {
+                    
+                    Shirt shirt = new Shirt(col, patt); // create  a new shirt object for each combo
+
+                    Console.WriteLine($"Shirt #{index}: {shirt}"); // call shirt ToString() method.
+                    index++; // name the shirts
+                }
+            }
+        }
+    }
+}
---
+++ html/48/Shirt.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class48_
+{
+    public enum Colors { 
+        Yellow,
+        Black,
+        White
+    }
+
+    public enum Patterns
+    {
+        Striped,
+        Checked,
+        Plain
+    }
+
+    internal class Shirt
+    {
+        public Colors Color { get; }
+        public Patterns Pattern { get;}
+
+
+        public Shirt(Colors color, Patterns pattern) // ctor
+        {
+            Color = color;
+            Pattern = pattern;
+        }
+
+        public override string ToString() // overwrites the ToString() method to print colors/patterns when calling print on Shirts
+        {
+            return $"{Color}, {Pattern}";
+        }
+
+
+    }
+}
